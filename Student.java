60872a12c80b035fa1395dfd045cc046a22f17ee
rushi7637 <rushikesh.patil@mitaoe.ac.in>
2024-02-09 19:30:21 +0530

    Fourth Commit

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating Student Object");
+    }
+    public void sayHello(){
+        System.out.println("hi,I am a student");
+    }
+    
+}
