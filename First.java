4aab5013c25f2bb08c77fadebb41f54b2c93862f
rushi7637 <rushikesh.patil@mitaoe.ac.in>
2024-02-09 19:02:43 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,9 @@
+import java.util.Date; // Import java.util.Date instead of java.sql.Date
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello jenkins");
+        System.out.println("this is first jenkins project");
+        System.out.println(new Date()); // Use java.util.Date
+    }
+}
